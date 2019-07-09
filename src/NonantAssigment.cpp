0ababe7ed1bca2b2e112824c0051c091a4f5f2f0
David Gabriel Monk <david.gabriel.monk@cern.ch>
2019-07-09 15:19:49 +0100

    Added visual studio code support

--- src/NonantAssigment.cpp
+++ src/NonantAssigment.cpp
@@ -4,8 +4,6 @@ NonantAssigment::NonantAssigment(void) {
     for (int i = 0; i < numPhiSecPerNon; i++) {
         phiSec.push_back((1 + 2*i - numPhiSecPerNon) * (phiSecWidth/2));
     }
-
-
 }
 
 NonantAssigment::~NonantAssigment(void) {
@@ -41,19 +39,14 @@ int NonantAssigment::assignModule(Module module) {
     } else {
         B = B_offset + B_slope * (fabs(z)/r);
     }
-    std::cout << A << " " << B << '\n';
 
     float C = rT*(1/r);
     float D = A*B;
-    std::cout << C << " " << D << '\n';
 
     float phiRes = abs(rT) * BconvPt;
     float estPhiRes = BendCut * D * fabs(C);
-    std::cout << estPhiRes << '\n';
     estPhiRes = (estPhiRes < phiSecWidth/2) ? estPhiRes : phiSecWidth/2;
-    std::cout << phiRes << " " << estPhiRes << '\n';
-    std::cout << std::endl;
     // float deltaPhi = phi0 - phiSec[0];
     // float deltaEstPhi = deltaPhi + bend * D * C;
-    // return D > 0;
+    return D > 0;
 }
